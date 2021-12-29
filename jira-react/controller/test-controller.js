9ac670273af51bbef3586a41585e1dac250e211e
osiris <osirisascanio02@gmail.com>
2021-12-28 23:32:56 -0400

    test schema init

--- jira-react/controller/test-controller.js
+++ jira-react/controller/test-controller.js
@@ -1,9 +1,18 @@
+const  Text  = require("../models/test")
 
 
 const getTest = async (req, res) => {
-    console.log(req.body)
-    // const {name} = req.body
-    res.status(200).json({ ok: "ok"})
+    
+     const {name} = req.body
+
+     const test = new Text({name})
+     await test.save()
+
+     if(!test){
+         res.status(500).json({err:"err"})
+     }
+
+    res.status(200).json({ ok: "ok", test})
 }
 
 module.exports = {
